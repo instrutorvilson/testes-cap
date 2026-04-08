92b853cd59a0d433b0223b5f7e70511acd77fa5a
instrutorvilson <instrutorvilsonmoro@gmail.com>
2026-04-08 08:11:41 -0300

    Adicionado testes para adicionar usuário

--- src/test/java/treina/testes/TesteApi/UsuarioControllerTest.java
+++ src/test/java/treina/testes/TesteApi/UsuarioControllerTest.java
@@ -3,6 +3,7 @@ package treina.testes.TesteApi;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class UsuarioControllerTest extends TestesApiBase{
     @Test
@@ -22,5 +23,40 @@ public class UsuarioControllerTest extends TestesApiBase{
                 .get("/v1/usuarios")
                 .then()
                 .statusCode(200);
+    }
+
+    @Test
+    void deveCriarUsuario() {
+        String json = """
+                            {
+                                "nome": "João",
+                                "email": "joao@email.com"
+                            }
+                       """;
+        given()
+                .auth().basic("admin", "123")
+                .contentType("application/json")
+                .body(json)
+                .when()
+                .post("/v1/usuarios")
+                .then()
+                .statusCode(201)
+                .body("nome", equalTo("João"));
+    }
+    @Test
+    void deveRetornar401AoCriarUsuarioSemAutenticacao() {
+        String json = """
+                            {
+                                "nome": "João",
+                                "email": "joao@email.com"
+                            }
+                       """;
+        given()
+                .contentType("application/json")
+                .body(json)
+                .when()
+                .post("/v1/usuarios")
+                .then()
+                .statusCode(401);
     }
 }
